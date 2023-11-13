6a4a867fd5c7935679bb380dd6a1c69ef59407ca
Srishti918 <srishtimalaviya123@gmail.com>
2023-11-13 21:02:39 +0530

    Added DB

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
 function App() {
   return (
     <div >
       <Navbar />
+      <Routes>
+        <Route path='/' elementt={<Home />} />
+      </Routes>
     </div>
   );
 }
