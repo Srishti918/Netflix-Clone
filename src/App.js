93b27f162ebe8fd306cfbd1a543b8350c1b3a269
Srishti918 <srishtimalaviya123@gmail.com>
2023-11-20 20:13:11 +0530

    added context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 function App() {
   return (
-    <div >
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
