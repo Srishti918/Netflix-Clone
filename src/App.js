f67e6d47f59a694312e2b517995906c827bbd0a9
Srishti918 <srishtimalaviya123@gmail.com>
2023-11-22 21:59:33 +0530

    updated login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import LogIn from "./pages/LogIn";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
         </Routes>
       </AuthContextProvider>
     </>
