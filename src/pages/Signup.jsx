d3aee330530fdec348e16e1ab6efede0edc5e499
Srishti918 <srishtimalaviya123@gmail.com>
2023-12-10 19:31:13 +0530

    updated signUp

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
-import { Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className='w-full h-screen'>
