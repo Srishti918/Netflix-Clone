bda70c60d6e5136b5bda174da4412b713efd2805
Srishti918 <srishtimalaviya123@gmail.com>
2023-12-11 18:04:58 +0530

    updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from '../context/AuthContext';
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
@@ -9,6 +11,18 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
+      {user?.email ? (
+        <div>
+          <Link to='/account'>
+            <button className='text-white pr-4'>Account</button>
+          </Link>
+          <button
+            className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
       <div>
         <Link to="/login">
           <button className="text-white pr-4">Sign In</button>
@@ -19,6 +33,7 @@ const Navbar = () => {
           </button>
         </Link>
       </div>
+      )}
     </div>
   );
 };
